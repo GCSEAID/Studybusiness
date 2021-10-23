7cf96f9f612571af557b3495bbfba48503e96858
FireBoyAJ24 <63443017+FireBoyAJ24@users.noreply.github.com>
2021-10-23 08:11:02 +0300

    Update script.js

--- static/cases/js/script.js
+++ static/cases/js/script.js
@@ -36,7 +36,7 @@ function pageLoaded() {
     const urlSearchParams = new URLSearchParams(window.location.search);
     const questions = document.getElementsByClassName("question");
     let halfwayThrough = Math.floor(questions.length / 2);
-    questions = questions.slice(halfwayThrough, yourArray.length);
+    questions = questions.slice(halfwayThrough, questions.length);
     // Display the params from the querystring
     console.log(params);
     console.log(questions);
