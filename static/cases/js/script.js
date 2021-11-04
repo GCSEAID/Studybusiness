815181688871b2341eded6ce859d9316d26c370e
FireBoyAJ24 <63443017+FireBoyAJ24@users.noreply.github.com>
2021-11-04 18:40:46 +0300

    Update script.js

--- static/cases/js/script.js
+++ static/cases/js/script.js
@@ -1,7 +1,7 @@
 var ans = document.getElementsByClassName("question")
 var urlSearchParams = new URLSearchParams(window.location.search);
 var params = Object.fromEntries(urlSearchParams.entries());
-var url = `${window.location.protocol}//${window.location.host}/${window.location.pathname}${window.location.search}`
+var url = `${window.location.protocol}//${window.location.host}${window.location.pathname}${window.location.search}&`
 var notif = document.getElementById("notif");
 
 // console.log(a.concat("/?","q1=","test"))
