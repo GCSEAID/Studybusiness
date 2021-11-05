57e9a9d886b5318ce87d945fc8c8e006230ea9dd
FireBoyAJ24 <63443017+FireBoyAJ24@users.noreply.github.com>
2021-11-05 06:46:34 +0300

    Organising code

--- static/cases/js/script.js
+++ static/cases/js/script.js
@@ -30,16 +30,55 @@ function save() {
 }
 
 
+function populateAnswer() {
+    // Get the params from the query string
+    const urlSearchParams = new URLSearchParams(window.location.search);
+    var params = Object.fromEntries(urlSearchParams.entries());
+
+    // Input variable
+    const answers = document.getElementsByClassName("question");
+
+    // populate the form fields
+    for (var i = 1; i < answers.length; i++) {
+        let q = "q" + i;
+        console.log(params[q])
+        answers[i-1].value = params[q] || "";
+    }
+}
+
+function populateValues() {
+    //Inserts title, description and question title values from the caseStudy Object
+    title.innerText =  caseStudy[id_web_url].title
+    desc.innerText =  caseStudy[id_web_url].desc
+    questionTitle.innerText = 'Questions For ' + title.innerText
+
+    //Duplicates and then deletes question divs
+    for (i = 0; i < caseStudy[id_web_url].questions.length; i++) {
+        var original = document.getElementById('duplicator');
+        var clone = original.cloneNode(true);
+        clone.id = i;
+        original.parentNode.appendChild(clone)
+    }
+    item.remove(item) //deletes duplicator
+
+    for (i = 0; i < caseStudy[id_web_url].questions.length; i++) {
+        var c = i+1
+        questions[i].parentElement.firstElementChild.innerHTML = "<strong>Question " + [c] + ":</strong> " + caseStudy[id_web_url].questions[i];
+    }
+
+    console.log("Body Loaded");
+}
 
 function pageLoaded() {
+    // Each one gets access to all the input areas on the HTML for the values to go into
     const urlIdParams = window.location.search;
 	const id_web_url = String(urlIdParams)[4] - 1;
-
     var questions = document.getElementsByClassName("question")
     var title = document.querySelector("#caseTitle");
     var desc = document.querySelector("#caseDesc");
     var questionTitle = document.querySelector("#caseQuestionsTitle");
-    
+    var item = document.getElementById("duplicator"); //Selects duplicator div to be later removed (unnecessary div at the end of the page)
+
     const caseStudy = [{
         id: '1',
         title: "Goods and Services, Air Vigour: Business Activity",
@@ -57,39 +96,13 @@ function pageLoaded() {
 `]      
     }]
 
-    title.innerText =  caseStudy[id_web_url].title
-    desc.innerText =  caseStudy[id_web_url].desc
-    questionTitle.innerText = 'Questions For ' + title.innerText
+    //Populate the html with case study information
+    populateValues(id_web_url, questions, title, desc, questionTitle, item, caseStudy)
+    
 
-    for (i = 0; i < caseStudy[id_web_url].questions.length; i++) {
-        var original = document.getElementById('duplicator');
-        var clone = original.cloneNode(true);
-        clone.id = i;
-        original.parentNode.appendChild(clone)
-    }
-    var item = document.getElementById("duplicator");
-    item.remove(item)
-
-    for (i = 0; i < caseStudy[id_web_url].questions.length; i++) {
-        var c = i+1
-        questions[i].parentElement.firstElementChild.innerHTML = "<strong>Question " + [c] + ":</strong> " + caseStudy[id_web_url].questions[i];
-    }
-
-    console.log("Body Loaded");
-
-    // Get the params from the query string
-    const urlSearchParams = new URLSearchParams(window.location.search);
-    var params = Object.fromEntries(urlSearchParams.entries());
-
-    // Input variable
-    const answers = document.getElementsByClassName("question");
-
-    // populate the form fields
-    for (var i = 1; i < answers.length; i++) {
-        let q = "q" + i;
-        console.log(params[q])
-        answers[i-1].value = params[q] || "";
-    }
+    // If answers are avaialbe then the populate function deals with that
+    populateAnswer()
+    
 }
 
 
