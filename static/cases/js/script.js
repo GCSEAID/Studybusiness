404862332a1beba3c38a4b0d7da7f81ee83f2758
FireBoyAJ24 <63443017+FireBoyAJ24@users.noreply.github.com>
2021-11-26 06:31:56 +0300

    Update script.js

--- static/cases/js/script.js
+++ static/cases/js/script.js
@@ -109,6 +109,23 @@ function pageLoaded() {
 }
 
 function caseStudyPagesLoaded(id_web_url, questions, title, desc, questionTitle, item, caseStudy) {
+    var caseStudy = [{
+        id: '1',
+        title: "Goods and Services, Air Vigour: Business Activity",
+        desc: `Air Vigour is a Spanish public limited company which has most of its factories in Spain.  Air Vigour produces air conditioners, refrigerator and other electric appliance. Air Vigour is one of the best-selling air conditioner appliances in Africa and Europe. 
+      =The Spanish company have 3 factories in China; 1 factory in Nairobi Kenya; and 4 factories in Bilbao, Spain. Air Vigour has maintained a continuous growth in sales by 3%. Air Vigour has invested its retained profits into building smartphones in 2018.  Air Vigour has invested in factories in India and R&D to support this expansion into the smartphones market.  In addition, to this investment Air Vigour has focused on the customer service. This is because there have been various complaints on the Air Vigour’s customer service.`,
+        questions: ["Define Goods and services. (2 marks)", "Define a Business. (2 marks)", "Why is it important for Air Vigour PLC remain profitable to their stakeholders? (3 marks)","Who are the most important stakeholders to Air Vigour and explain why? (6 marks)","Discuss the importance of having a clear business of objective in a changing environment? (9 marks)"]
+    }, {
+      id: "2",
+      title: "Omar Sweet Shop: Business Objectives",
+      desc: `Omar owns a sweet shop in one of the busiest souqs in Kuwait. Omar most profitable months were in the Ramadan season where people fasted from sunrise to sunset. Omar’s family has also been helping in the running of the business. The sole trader has been thinking of starting a private limited company and finally integrate his family completely into the business. 
+    In the recent months Omar has been struggling to gain customers because of the increase in the VAT by 10%. This has caused Omar to have his first losses by 820.57 Kuwaiti Dinar. Omar has decided to change his objectives to survive until the Ramadan arrives. He has then planned to raise enough capital to buy a shop in a wealthy district of Kuwait City to gain more customers.`,
+        questions: ["What is the meaning of Objectives? (2 marks)", "Give me two types of objectives? (2 marks)", "From the Case Study, get one objective that Omar has set and explain why that objective is a good or bad one. (3 marks)", "Explain one reason why businesses change their objectives? (3 marks)", "List one stakeholder and explain their purpose in Omar’s business. (4 marks)", "What are the two advantages of setting objectives in Omar’s case? (6 marks)", `Discuss whether Omar’s objective stated below is a good objective:
+    “From today onwards until the start of Ramadan we shall focus only on making the business survive."
+    (9 marks)
+    `]      
+    }]
+
     //Inserts title, description and question title values from the caseStudy Object
     title.innerText =  caseStudy[id_web_url].title
     desc.innerText =  caseStudy[id_web_url].desc
