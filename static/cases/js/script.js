c1d7eb629b398f9a843e7c66de6f70b3dae8b075
FireBoyAJ24 <63443017+FireBoyAJ24@users.noreply.github.com>
2021-11-26 08:12:05 +0300

    Update script.js

--- static/cases/js/script.js
+++ static/cases/js/script.js
@@ -141,9 +141,9 @@ function caseStudyPagesLoaded(id_web_url, questions, title, desc, questionTitle,
 
     for (i = 0; i < length_case; i++) {
         var c = i+1
-        title[i].parentElement.innerText =  caseStudy[i].title
+        title[i].innerText =  caseStudy[i].title
         console.log(caseStudy[i].title);
-        desc[i].parentElement.firstElementChild.innerText =  caseStudy[i].des
+        desc[i].innerText =  caseStudy[i].desc
         // questions[i].parentElement.firstElementChild.innerHTML = "<strong>Question " + [c] + ":</strong> " + caseStudy[id_web_url].questions[i];
     }
     
