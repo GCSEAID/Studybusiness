e1a0359b8db8469ce4fd13c8122e45ec28b114e9
FireBoyAJ24 <63443017+FireBoyAJ24@users.noreply.github.com>
2021-11-26 14:25:23 +0300

    Added Chickeny Tendors Case Study!

--- static/public/js/StudyBusinessvar.js
+++ static/public/js/StudyBusinessvar.js
@@ -10,17 +10,31 @@ var caseStudy = [{
   author: "Akshanjay",
   image: `<img src="https://raw.githubusercontent.com/GCSEAID/Studybusiness/dev/static/public/images/Air%20Vigour%20Pic.jpg" alt="Image">`,
 }, {
-id: "2",
-chapter: "1",
-chapter_name: "Business",
-section_name: "Business Objectivies",
-title: "Omar Sweet Shop: Business Objectives",
-desc: `Omar owns a sweet shop in one of the busiest souqs in Kuwait. Omar most profitable months were in the Ramadan season where people fasted from sunrise to sunset. Omar’s family has also been helping in the running of the business. The sole trader has been thinking of starting a private limited company and finally integrate his family completely into the business. 
-In the recent months Omar has been struggling to gain customers because of the increase in the VAT by 10%. This has caused Omar to have his first losses by 820.57 Kuwaiti Dinar. Omar has decided to change his objectives to survive until the Ramadan arrives. He has then planned to raise enough capital to buy a shop in a wealthy district of Kuwait City to gain more customers.`,
+  id: "2",
+  chapter: "1",
+  chapter_name: "Business",
+  section_name: "Business Objectivies",
+  title: "Omar Sweet Shop: Business Objectives",
+  desc: `Omar owns a sweet shop in one of the busiest souqs in Kuwait. Omar most profitable months were in the Ramadan season where people fasted from sunrise to sunset. Omar’s family has also been helping in the running of the business. The sole trader has been thinking of starting a private limited company and finally integrate his family completely into the business. 
+  In the recent months Omar has been struggling to gain customers because of the increase in the VAT by 10%. This has caused Omar to have his first losses by 820.57 Kuwaiti Dinar. Omar has decided to change his objectives to survive until the Ramadan arrives. He has then planned to raise enough capital to buy a shop in a wealthy district of Kuwait City to gain more customers.`,
   questions: ["What is the meaning of Objectives? (2 marks)", "Give me two types of objectives? (2 marks)", "From the Case Study, get one objective that Omar has set and explain why that objective is a good or bad one. (3 marks)", "Explain one reason why businesses change their objectives? (3 marks)", "List one stakeholder and explain their purpose in Omar’s business. (4 marks)", "What are the two advantages of setting objectives in Omar’s case? (6 marks)", `Discuss whether Omar’s objective stated below is a good objective:
 “From today onwards until the start of Ramadan we shall focus only on making the business survive."
 (9 marks)
 `],
   author: "Akshanjay",
   image: `<img src="https://raw.githubusercontent.com/GCSEAID/Studybusiness/dev/static/public/images/Omar%20Sweet%20Shop%20Pic.jpg" alt="Image">`,
+}, {
+  id: "3",
+  chapter: "1",
+  chapter_name: "Business",
+  section_name: "Sole Traders",
+  title: "Chickeny Tendor: Sole Traders",
+  desc: `Rafid has seen his restraunt grow to a successive chicken restraint in KSA for the past 6 years. Chickeny Tendor is well distributed throughout KSA; 30 restaurants in total. However, in the recent years there has been a shallow decline in sales. Rafid believes that this cause is because of the rise in the number of multinational fast food joints.
+  Rafid has plans to build an online delivery service to the increasing popularity of delivery service. In-addition Rafid has been looking at diversifying his brand image into the drinks industry. He hopes that these investments will help ensure the future prosperity of Chickeny Tendor.`,
+  questions: ["Define multinational companies. (2 marks)", "Define AGM (2 marks)", "Provide reasons for why Rafid is wanting to diversify his business? (3 marks)", "What are the advantages of having multinationals to an economy? (6 marks)", `Rafid has plans in making Chickeny Tendor as private limited company and franchise his business outside KSA.
+  Choice 1: Chickeny Tendor becoming a Limited Liability company
+  Choice 2: Chickeny Tendor becoming a Franchise.
+  Choose from the option above and justify why you chose that option. (10 marks)`],
+  author: "Akshanjay",
+  image: `<img src="https://images.pexels.com/photos/2673353/pexels-photo-2673353.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="Image">`,
 }]
