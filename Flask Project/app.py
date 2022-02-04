ab2b0e6269ddb974db2decf723ee21c6ac07fcfe
Deniernal354 <danielarston@gmail.com>
2022-02-04 16:32:39 +0300

    testing heroku build

--- Flask Project/app.py
+++ Flask Project/app.py
@@ -1,3 +1,4 @@
+import os
 from flask import (Flask, make_response, render_template, Response) 
 from flask_cors import CORS
 
@@ -25,4 +26,6 @@ def template():
     return render_template("template.html")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    # app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
